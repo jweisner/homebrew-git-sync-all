246b1ddc345dabe8eb7588d4b1458372eee1f1c0
Jesse Weisner <jesse@weisner.ca>
2026-05-04 08:45:13 -0700

    Update git-sync-all to v1.2.1

--- Formula/git-sync-all.rb
+++ Formula/git-sync-all.rb
@@ -1,8 +1,8 @@
 class GitSyncAll < Formula
   desc "Scan a directory tree for git repos and keep them in sync"
   homepage "https://github.com/jweisner/git-sync-all"
-  url "https://github.com/jweisner/git-sync-all/archive/refs/tags/v1.2.0.tar.gz"
-  sha256 "ccf99b9b3efade22e17f92ff5df2235a4c1250785667977a3171fe58f6efc5bc"
+  url "https://github.com/jweisner/git-sync-all/archive/refs/tags/v1.2.1.tar.gz"
+  sha256 "98446c658da996a3bb654193329421749c90a24e458da14ace1f14daab8c81f6"
   license "Apache-2.0"
 
   def install
